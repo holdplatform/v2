648d073c1d11262ab332057b1598b49ecb7c0f43
holdplatform <54728552+holdplatform@users.noreply.github.com>
2024-12-06 08:00:50 +0700

    Update holdplatform.sol

--- holdplatform.sol
+++ holdplatform.sol
@@ -6,7 +6,7 @@ import "./openzeppelin/security/ReentrancyGuard.sol";
 import "./interfaces/ERC20Interface.sol";
 
 /* import "./openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol"; 
-    ⫸ This function is not used because it exceeds 24,576 bytes.
+    ⫸ This function is not used because it exceeds 24,576 bytes
 */
 
 
